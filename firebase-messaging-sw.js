6b7011f35dbddbb5646e4b39d6317eef4178f41c
mario-pitta <pitta_mario@hotmail.com>
2018-02-21 01:05:19 -0300

    push notifications

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
+firebase.initializeApp({
+    "messagingSenderId": "998390484019"
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload){
+    console.log('Messagem recebida de background');
+    return self.registration.showNotification({}, {});
+})
+
---
+++ js/push.js
@@ -0,0 +1,26 @@
+console.log("[App] Started Push Listening");
+
+const messaging = firebase.messaging();
+
+messaging.requestPermission().then(function(){
+  console.log('Permisão Concedida');
+
+  return messaging.getToken().then(function (currentToken){
+    if (currentToken){
+      console.log(currentToken);
+      return currentToken;
+    }else{
+      console.warn("Nenhum ID disponivel. Solicite Permissão ao ADM");
+    }
+  })
+});
+
+messaging.getToken().then(function (currentToken){
+    if(currentToken){
+      console.log(currentToken);
+      return currentToken;
+    }else{
+      console.warn("Nenhum ID disponivel. Solicite Permissão ao ADM");
+    }  }).cath(function (err){
+    console.warn('Erro ao gerar Novo Token', err);
+  })
